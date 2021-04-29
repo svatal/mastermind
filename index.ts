109c58af46f2e2aba1884400f5e1c7149395a37d
svatal <swata@centrum.cz>
2021-04-29 22:43:49 +0200

    best first move
    - validated solution of "1122" for 6 colors, 4 pos with repetition (Knuth's best first move)
    - for 8c5p wo repetition it should be "11123" reducing the problem space more than 4 times

--- index.ts
+++ index.ts
@@ -1,10 +1,53 @@
-import { generateAll } from "./generator";
+import {
+    generateAll,
+    generateAllSignificantWithRepetition,
+    getColorGroups,
+} from "./generator";
 import { match } from "./matcher";
 
-const all = generateAll({ colorCount: 8, positionCount: 5, repetition: false });
-console.log(all.length);
-const matched = all
-    .map((c) => match(c, "11345"))
-    .map(({ exact, color }) => `${exact}-${color}`)
-    .reduce((p, c) => p.set(c, (p.get(c) || 0) + 1), new Map<string, number>());
-console.log(matched);
+const colorCount = 8;
+const positionCount = 5;
+const repetition = false;
+
+// const colorCount = 6;
+// const positionCount = 4;
+// const repetition = true;
+
+const all = generateAll({ colorCount, positionCount, repetition });
+console.log("all", all.length);
+
+const significant = generateAllSignificantWithRepetition(
+    positionCount,
+    getColorGroups([], colorCount)
+);
+console.log("significant", significant.length);
+
+let bestCandidate = "";
+let bestValue = all.length + 1;
+let bestSplit = new Map<string, string[]>();
+significant.forEach((candidate) => {
+    const s = split(candidate, all);
+    const v = getSplitValue(s);
+    console.log("split", candidate, v, s);
+    if (v < bestValue) {
+        bestValue = v;
+        bestSplit = s;
+        bestCandidate = candidate;
+    }
+});
+console.log("best", bestCandidate, bestValue, bestSplit);
+
+function split(question: string, possibilities: string[]) {
+    return possibilities
+        .map((c) => ({ c, match: match(c, question) }))
+        .map(({ c, match }) => ({ c, match: `${match.exact}-${match.color}` }))
+        .reduce((p, { c, match }) => {
+            if (!p.has(match)) p.set(match, []);
+            p.get(match)!.push(c);
+            return p;
+        }, new Map<string, string[]>());
+}
+
+function getSplitValue(split: Map<string, string[]>) {
+    return Math.max(...Array.from(split.values()).map((v) => v.length));
+}
