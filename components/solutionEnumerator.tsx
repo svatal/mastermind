7bff85e6673ac2debebecc23af93ee7d72a44b03
svatal <swata@centrum.cz>
2021-05-23 09:17:14 +0200

    better way to apply the pause
    .. and also correctly stops the timeout on dispose

--- components/solutionEnumerator.tsx
+++ components/solutionEnumerator.tsx
@@ -1,5 +1,4 @@
 import * as b from "bobril";
-import { useRef } from "bobril";
 import { generateAll } from "../generator";
 import { IOptions } from "../settings";
 import { IBestSplit } from "../solutionEnumerator";
@@ -19,12 +18,8 @@ export function SolutionEnumerator(p: {
     const root = b.useStore(() => ({ size: all.length } as IState));
     const toDo = b.useStore(() => [root]);
     const [steps, setSteps] = b.useState(0);
-    const paused = useRef(p.paused);
-    paused(p.paused); // pass the paused property into oneStep callback
+    const timeoutRef = b.useRef<ReturnType<typeof setTimeout>>(undefined);
     const oneStep = b.useCallback(() => {
-        if (paused.current) {
-            return;
-        }
         const current = toDo.pop();
         if (!current) {
             return;
@@ -32,10 +27,15 @@ export function SolutionEnumerator(p: {
         const processedState = step(all, p.options, p.getBestSplit(), current);
         toDo.push(...processedState.children);
         setSteps((steps) => steps + 1); // invalidate
-        setTimeout(oneStep);
+        timeoutRef.current = setTimeout(oneStep);
     }, []);
     b.useEffect(() => {
         !p.paused && oneStep();
+        return () => {
+            if (timeoutRef.current) {
+                clearTimeout(timeoutRef.current);
+            }
+        };
     }, [p.paused]); // start the evaluation
     return (
         <div>
